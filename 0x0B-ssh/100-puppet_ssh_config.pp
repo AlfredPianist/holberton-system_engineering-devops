a3245882d9d7cb2f0bde3fee6e4b27ddbdbc7f2d
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-08-14 02:00:27 +0000

    Add: tasks for 0x0B

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Turns off password authentication and declares identity file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  match  => '^#\s\s\sPassword'
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton'
+}
