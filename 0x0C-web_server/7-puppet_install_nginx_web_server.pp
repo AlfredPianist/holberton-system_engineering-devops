2c307419811174697c6be2fdb8a50b7f979ef4dd
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-08-17 21:08:50 +0000

    Fix: Update and upgrade commands for 7 from 0x0C

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,10 +1,23 @@
 # Script that installs and configures nginx on a server
 
+# Apt-get update
+exec { 'apt-update':
+  command => 'apt-get update',
+  path    => ['/usr/bin', '/bin', '/usr/sbin']
+}
+
+# Apt-get upgrade
+exec { 'apt-upgrade':
+  command => 'apt-get upgrade -y',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  require => Exec['apt-update']
+}
+
 # Install nginx
 package { 'nginx':
   ensure  => 'installed',
   name    => 'nginx',
-  require => Exec['apt-get update', 'apt-get upgrade']
+  require => Exec['apt-upgrade']
 }
 
 # Create index file with content "Holberton School for the win!"
