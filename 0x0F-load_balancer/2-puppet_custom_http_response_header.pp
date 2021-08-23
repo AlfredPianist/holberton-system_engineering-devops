1ab2da54b8414f1a894a87f0f382f9539aaa843f
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-08-23 21:33:38 +0000

    Fix: change puppet file 0x0F

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,36 +1,32 @@
 # Script that installs and configures nginx on a server
 
 # Install nginx
-package { 'nginx':
-  ensure  => 'installed',
+exec { 'nginx':
+  command  => 'sudo apt update &&
+	      sudo apt install nginx -y',
+  provider => shell
 }
 
 # Create index file with content "Holberton School for the win!"
 file { '/var/www/html/index.html':
   content => 'Holberton School for the win!',
-  require => Package['nginx']
+  require => Exec['nginx']
 }
 
-# Add custom header 
-file_line { 'custom-header':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'server_name _;',
-  line    => 'add-header X-Served-By "$HOSTNAME";',
-  require => Package['nginx']
-}
-
-# Add permament redirection
-file_line { 'redirection-301':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'add-header X-Served-By "$HOSTNAME";',
-  line    => 'rewrite ^/redirect_me https://example.com/ permanent;',
-  require => Package['nginx']
+# Add custom header and permanent redirection
+exec { 'modify-default':
+  command  => 'sudo sed -i "/server_name _;/a \\\n\\tadd_header X-Served-By $HOSTNAME;"\
+     		/etc/nginx/sites-available/default;
+	       sudo sed -i "/add_header*/a \\\n\\trewrite ^/redirect_me https://example.com/ permanent;"\
+     		/etc/nginx/sites-available/default;
+	       sudo sed -i "/rewrite*/a \\\n\\terror_page 404 /404.html;\\n\\n\\tlocation = /404.html {\\n\\t\\tinternal;\\n\\t}"\
+     		/etc/nginx/sites-available/default;',
+  provider => shell,
+  require  => Exec['nginx']
 }
 
 # Run service nginx
 service { 'nginx':
   ensure  => 'running',
-  require => Package['nginx']
+  require => Exec['modify-default']
 }
