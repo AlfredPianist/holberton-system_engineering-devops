70edd06f3200afe25d0398b4555a3335ff68f792
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-09-27 22:10:47 +0000

    Fix: one recursive function

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,65 +2,44 @@
 """
 100-count: Counts the number off occurrences of words from a given subreddit.
 """
-import requests
+from requests import get
 
 
-def count_words(subreddit, word_list):
+def count_words(subreddit, word_list, payload={}, hot_list=[]):
     """Counts the number of occurrences of words given as list in
     the titles of all hot topics from a given subreddit"""
+    if payload == {}:
+        payload = {'limit': 100}
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     user_agent_str = ('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0)'
                       'Gecko/20190101 Firefox/77.0')
     user_ag = {'User-Agent': user_agent_str}
-    session = requests.Session()
-    session.headers.update(user_ag)
 
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    request = session.get(url)
+    request = get(url, headers=user_ag)
     if (request.status_code == 404):
         return
 
-    payload = {'limit': 100}
-    titles = recurse(session, url, payload, [])
-
-    low_list = []
-    multiply = []
-    uniq_list = []
-    for word in word_list:
-        low_list.append(word.lower())
-    for word in low_list:
-        if word not in uniq_list:
-            uniq_list.append(word)
-            multiply.append(low_list.count(word))
-
-    n = len(uniq_list)
-    quantity = [None] * n
-
-    for i in range(n):
-        number = 0
-        for title in titles:
-            number += title.lower().split().count(uniq_list[i])
-        quantity[i] = number * multiply[i]
-
-    ans = list(zip(uniq_list, quantity))
-    ans_ordened = sorted(ans, key=lambda x: x[1], reverse=True)
-    for name, quantity in ans_ordened:
-        if quantity:
-            print("{}: {}".format(name, quantity))
-    return titles
-
-
-def recurse(session, url, payload, hot_list):
-    """Stores the titles of all hot topics in the actual subreddit"""
-    request = session.get(url, params=payload, allow_redirects=False)
+    request = get(url, headers=user_ag, params=payload)
     request_data = request.json().get('data')
 
     post_page = request_data.get('children')
     hot_list.extend([post.get('data').get('title') for post in post_page])
 
-    if (request_data.get('after') is None):
-        return hot_list
-    payload = {
-        'after': request_data.get('after'),
-        'limit': 100
-    }
-    return recurse(session, url, payload, hot_list)
+    if (request_data.get('after') is not None):
+        payload = {
+            'after': request_data.get('after'),
+            'limit': 100
+        }
+        return count_words(subreddit, word_list, payload, hot_list)
+
+    word_list = [word.lower() for word in word_list]
+    freq_dict = {word: 0 for word in word_list}
+    for title in hot_list:
+        title_words = title.lower().split(' ')
+        for word in word_list:
+            freq_dict[word] += title_words.count(word)
+
+    for key in sorted(freq_dict):
+        if (freq_dict[key] != 0):
+            print("{}: {}".format(key, freq_dict[key]))
