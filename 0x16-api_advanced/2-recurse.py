2710f75deeaf0f80a5b004e5b3f295cfa7fd87b8
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-09-27 19:04:53 +0000

    Fix: user_agent as str

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,7 +9,9 @@ import user_agent
 
 def recurse(subreddit, hot_list=[]):
     """Gets the top ten hot posts from a given subreddit"""
-    user_ag = {'User-Agent': user_agent.generate_user_agent()}
+    user_agent_str = ('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0)'
+                      'Gecko/20190101 Firefox/77.0')
+    user_ag = {'User-Agent': user_agent_str}
     session = requests.Session()
     session.headers.update(user_ag)
 
