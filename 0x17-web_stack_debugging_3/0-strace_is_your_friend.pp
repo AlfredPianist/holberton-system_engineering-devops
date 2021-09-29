897ec3bf8109a77c612878a401a06209bc99225f
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-09-29 22:33:03 +0000

    Fix: Sed version

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,6 @@
 # Fixes typo phpp
 
-file_line { 'fix phpp':
-  ensure => 'present',
-  path   => '/var/www/html/wp-settings.php',
-  line   => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
-  match  => 'phpp'
+exec { 'fix phpp':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => ['/usr/bin', '/bin']
 }
